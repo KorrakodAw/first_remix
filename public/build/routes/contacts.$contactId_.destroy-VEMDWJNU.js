a41444064718171621517e876a2b5e4f2761fc95
KorrakodAw <karakod22@gmail.com>
2025-04-03 12:07:07 +0700

    Add all missing files

---
+++ public/build/routes/contacts.$contactId_.destroy-VEMDWJNU.js
@@ -0,0 +1,12 @@
+import {
+  __commonJS
+} from "/build/_shared/chunk-PNG5AS42.js";
+
+// browser-route-module:routes/contacts.$contactId_.destroy.tsx?browser
+var require_contacts_contactId_destroy = __commonJS({
+  "browser-route-module:routes/contacts.$contactId_.destroy.tsx?browser"(exports, module) {
+    module.exports = {};
+  }
+});
+export default require_contacts_contactId_destroy();
+//# sourceMappingURL=/build/routes/contacts.$contactId_.destroy-VEMDWJNU.js.map
